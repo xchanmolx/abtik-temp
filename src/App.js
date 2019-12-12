8aff4e9fd21586b37896ef27e2fff30fee6d68ee
xchanmolx <cmanigos@gmail.com>
2019-12-12 18:32:10 +0800

    chanmerc-pc first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './containers/Home';
+import LayoutMain from './hoc/LayoutMain/LayoutMain';
+
+class App extends Component { 
+  
+  render() {    
+
+    let routes = (
+      <Switch>
+          <Route path="/" exact component={Home} />            
+      </Switch>  
+    );  
+
+    return (
+      <div>
+          <LayoutMain />
+      </div>
+    );
+  }  
 }
 
 export default App;
---
+++ src/hoc/LayoutMain/LayoutMain.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+class LayoutMain extends Component {
+    render () {
+        return (
+            <React.Fragment>
+                <CssBaseline />
+                <Container fixed>
+                    <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />             
+                </Container>
+            </React.Fragment>
+        );
+    }
+}
+
+export default LayoutMain;
