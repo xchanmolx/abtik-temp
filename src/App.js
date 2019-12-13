9723247a56b30c5be996f3f69d446b29c8bc271a
xchanmolx <cmanigos@gmail.com>
2019-12-13 13:20:58 +0800

    chanmerc-pc update template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React, {Component} from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Switch} from 'react-router-dom';
 
-import Home from './containers/Home';
-import LayoutMain from './hoc/LayoutMain/LayoutMain';
+import Layout from './hoc/Layout/Layout';
+import Home from './containers/Home/Home';
+import Project from './containers/Project/Project';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 
-class App extends Component { 
-  
-  render() {    
+class App extends Component {
 
-    let routes = (
-      <Switch>
-          <Route path="/" exact component={Home} />            
-      </Switch>  
-    );  
-
+  render() {
+    
     return (
       <div>
-          <LayoutMain />
+        <Layout>          
+          <Switch>            
+            <Route path="/project" component={Project} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/" exact component={Home} />            
+          </Switch>
+        </Layout>
       </div>
     );
-  }  
+  }
 }
 
 export default App;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+
     render () {
+        
         return (
             <div>
                 <h1>Home</h1>
